d52a7a8e264ce7e0093d31f97bfbeb58f9bf0d3b
Oliver Nybroe <olivernybroe@gmail.com>
2019-11-21 15:49:04 +0100

    chore(workflow): Test if workflow will run

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 echo "::set-output name=conflicts::"
 
 if
-    grep -lr -R --exclude-dir={node_modules,vendor,.git} --exclude=conflictFinder '<<<<<<<' .;
+    grep -lr -r --exclude-dir={node_modules,vendor,.git} --exclude=conflictFinder '<<<<<<<' .;
 then
     printf "\033[1;31mFound merge conflicts.\033[0m\n"
     exit 1;
